8a004eb6f5fd811e5c51919f350209f50464df80
facily <thyago.pereira@faci.ly>
2021-12-07 16:35:59 -0300

    Ajustando questoes de autorizaçao

--- app/repositories/user_repository.py
+++ app/repositories/user_repository.py
@@ -9,4 +9,4 @@ class UserRepository(BaseRepository):
         super().__init__(session, User)
 
     def find_by_email(self, email):
-        return self.session.query(self.model).filter_by(email=email).frist()
+        return self.session.query(self.model).filter_by(email=email).first()
