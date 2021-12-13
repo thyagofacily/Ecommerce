22bcf022a6dfa778e84b13965ff1c9429995a456
facily <thyago.pereira@faci.ly>
2021-12-13 14:16:30 -0300

    Refatoraçao

--- app/api/category/views.py
+++ app/api/category/views.py
@@ -1,5 +1,7 @@
 from typing import List
 from fastapi import APIRouter, status, Depends
+
+from app.repositories.category_repository import CategoryRepository
 from .schemas import CategorySchema, ShowCategorySchema
 from app.db.db import get_db
 from sqlalchemy.orm import Session
@@ -9,23 +11,17 @@ from app.services.auth_service import get_user, only_admin
 router = APIRouter(dependencies=[Depends(only_admin)])
 
 @router.post('/', status_code=status.HTTP_201_CREATED)
-def create(category: CategorySchema, db: Session = Depends(get_db)):
-    db.add(Category(**category.dict()))
-    db.commit()
-
-
+def create(category: CategorySchema, repository: CategoryRepository = Depends()):
+    repository.create(Category(**category.dict()))
+    
 @router.get('/', response_model=List[ShowCategorySchema])
-def index(db: Session = Depends(get_db)):
-    return db.query(Category).all()
-
+def index(repository: CategoryRepository = Depends()):
+    return repository.get_all()
 
 @router.put('/{id}')
-def update(id: int, category: CategorySchema, db: Session = Depends(get_db)):
-    query = db.query(Category).filter_by(id=id)
-    query.update(category.dict())
-    db.commit()
-
+def update(id: int, category: CategorySchema, repository: CategoryRepository = Depends()):
+    return repository.update(id, category.dict())
 
 @router.get('/{id}', response_model=ShowCategorySchema)
-def show(id: int, db: Session = Depends(get_db)):
-    return db.query(Category).filter_by(id=id).first()
+def show(id: int, repository: CategoryRepository = Depends()):
+    return repository.get_by_id(id)
